717f0d3f414dfc53295ace37406df65e991df43c
Sebastian Pipping <sebastian@pipping.org>
2017-05-03 18:49:01 +0200

    Bump version to 0.7.3

--- modules/fetchcommandwrapper/version.py
+++ modules/fetchcommandwrapper/version.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2010 Sebastian Pipping <sebastian@pipping.org>
+# Copyright (C) 2010-2017 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under GPL v3 or later
 
-VERSION = (0, 7, 2)
+VERSION = (0, 7, 3)
 VERSION_STR = '.'.join(str(e) for e in VERSION)
