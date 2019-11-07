53072266f05d34ca9fc2402bab6aca96a4b6f0e3
Sebastian Pipping <sebastian@pipping.org>
2019-11-07 14:09:34 +0100

    Bump version to 0.8.2

--- modules/fetchcommandwrapper/version.py
+++ modules/fetchcommandwrapper/version.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2010-2017 Sebastian Pipping <sebastian@pipping.org>
 # Licensed under GPL v3 or later
 
-VERSION = (0, 8, 1)
+VERSION = (0, 8, 2)
 VERSION_STR = '.'.join(str(e) for e in VERSION)
