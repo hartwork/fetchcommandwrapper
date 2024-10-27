cffc0b45433e4997ce483ea61a9ecb403d5c9cb7
Sebastian Pipping <sebastian@pipping.org>
2024-10-27 02:16:59 +0200

    Make GitHub Actions run smoke tests

---
+++ .github/workflows/dummy-portageq.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+mirrors=(
+    http://ftp.spline.inf.fu-berlin.de/mirrors/gentoo/
+    http://ftp-stud.hs-esslingen.de/pub/Mirrors/gentoo/
+    http://ftp.uni-erlangen.de/pub/mirrors/gentoo/
+    http://ftp.halifax.rwth-aachen.de/gentoo/
+    http://linux.rz.ruhr-uni-bochum.de/download/gentoo-mirror/
+    ftp://ftp.wh2.tu-dresden.de/pub/mirrors/gentoo/
+    ftp://sunsite.informatik.rwth-aachen.de/pub/Linux/gentoo/
+    ftp://ftp.tu-clausthal.de/pub/linux/gentoo/
+)
+echo "${mirrors[@]}"
